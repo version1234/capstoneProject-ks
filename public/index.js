8e5f0bb4c1fd311948ae3e5366c16d263d12ffb8
suneetha kanumurit <suneetaraj@gmail.com>
2022-09-09 20:33:22 -0500

    recommit

--- public/index.js
+++ public/index.js
@@ -6,6 +6,7 @@ const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const nameInput = document.querySelector('#username')
 const submitBtn = document.querySelector('#submit_button');
+const errorDiv = document.querySelector('#errorDiv');
 
 const getSubmit = document.getElementById('getSubmit');
 
@@ -16,13 +17,22 @@ const responseSection = document.getElementsByClassName('response-area')[0];
 const getValidateLogin = () => {
 console.log("RRRRRRRRgetValidateLogin");
 console.log(usernameInput.value);
-    axios.get(`http://localhost:5051/login/${usernameInput.value}`)
+    axios.get(`http://localhost:5050/login/${usernameInput.value}`)
     .then(res => {
-        console.log("result ::" , res.data);
-        res.data.forEach(login => {
-            console.log(login);
 
-        })
+        console.log("result ::" , res.data);
+        // res.data.forEach(login => {
+        //     console.log(login);
+
+        // })
+        console.log(res.data[0].username);
+        //console.log(res.length)
+    
+        if(res.data[0].password === passwordInput.value ){
+            console.log(" password  is  equal ");
+        }else {
+            errorDiv.innerHTML = '<div><h5>Credentials are NOT valid. Please re-enter</h5></div>';
+        }  
     })
 }
 
