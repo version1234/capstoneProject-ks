a56a8f8874441e807ceceb5aea009bf24187b08b
suneetha kanumurit <suneetaraj@gmail.com>
2022-09-13 15:34:46 -0500

    deploy

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const axios = require("axios");
  const path = require('path')
  const app = express();
- const SERVER_PORT = 5050
+ //const SERVER_PORT = 5050
 const {seed, getLogins,  bookAppointment, listAppointments, getLogin, getstates, getavailableMedicalServicesByState, getavailableMedicalServicesById} = require('./controller.js')
  app.use(express.json())
  app.use(cors());
@@ -23,5 +23,20 @@ app.get('/availableMedicalServices/:id', getavailableMedicalServicesById)
 app.post('/appointment', bookAppointment)
 app.get('/appointments',listAppointments)
 
- 
- app.listen(SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}`));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname,'../avaServices.html'));
+});
+
+// app.get('/styles', function(req, res) {
+//     res.sendFile(path.join(__dirname,'/public/styles.css'));
+// });
+
+// app.get('/js', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/avaServices.js'));
+// });
+
+
+ const port = process.env.PORT || 5050;
+ app.listen(port, () => {console.log(`listening on port ${port}`);
+})
